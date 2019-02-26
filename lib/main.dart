016976ffe8cc8efdb2a56471d3989e3029b3df84
SachseInTheHub <nelsonsachse@gmail.com>
2019-02-26 01:01:49 +0000

    added network call to json added HeroData

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+
+import 'dart:convert';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
@@ -20,75 +25,56 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   MyHomePage({Key key, this.header, this.url}) : super(key: key);
   final String header;
   final String url;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> _heroes = [
-    "Spider-Man",
-    "Thor",
-    "Ironman",
-    "Hulk",
-    "Batman",
-    "Superman",
-    "Deadpool"
-  ];
-
-  List<String> _links = [
-    "https://cdn3.whatculture.com/images/2014/04/SuperiorSpiderMan_02.png",
-    "https://static.comicvine.com/uploads/original/9/96648/2834291-1265093_130_ultimate_comics_new_ultimates_4.jpg",
-    "http://www.popculart.com/uploads/1/0/5/7/105745419/hulk-6_orig.jpg",
-    "https://cdn.flickeringmyth.com/wp-content/uploads/2018/06/Deadpool-1-coverNotFinal.png",
-    "https://static3.srcdn.com/wordpress/wp-content/uploads/2018/08/Daredevil-sense.jpg",
-    "https://static.comicvine.com/uploads/original/11118/111183611/5193180-tumblr_nyxu5mbe891sbqrlmo1_1280.jpg",
-    "https://i.kinja-img.com/gawker-media/image/upload/s--EcFIUboD--/c_scale,f_auto,fl_progressive,q_80,w_800/18mg4fh2v8emrjpg.jpg"
-  ];
-
-  String heroOrVilan(int i) {
-    return i.isEven ? 'Hero 🦸🏻‍♀️' : "Vilan 🦹🏼‍♂️";
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    // _heroes = List.generate(100, (i) => '${heroOrVilan(i)}');
-    _heroes = List.generate(100, (i) => '${heroOrVilan(i)}');
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.header),
-        ),
-        body: Center(
-            child: ListView(padding: EdgeInsets.all(8.0),
-              children: _links.map((url) => Image.network(url, height: 200,fit: BoxFit.fitWidth)  ).toList())));
-            // child: ListView(children: <Widget>[Image.network(widget.url)])));
+      appBar: AppBar(
+        title: Text(header),
+      ),
+      body: FutureBuilder<List<HeroData>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
+
+          return snapshot.hasData
+              ? ListView(
+                  padding: EdgeInsets.all(8.0),
+                  children: snapshot.data
+                      .map((json) => Image.network(json.url,
+                          height: 200, fit: BoxFit.fitWidth))
+                      .toList())
+              : Center(child: CircularProgressIndicator());
+        },
+      ),
+    );
   }
 }
 
-// class HeroesCards extends StatefulWidget {
-//   String _url = "";
+Future<List<HeroData>> fetchPhotos(http.Client client) async {
+  final response =
+      await client.get('https://api.jsonbin.io/b/5c72ff3f3f18a135eaf15509/4');
 
-//   @override
-//   State<StatefulWidget> createState() => HeroesCard(_url);
+  return compute(parseHeroData, response.body);
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       child: Column(
-//         children: <Widget>[
-//           Image.network(_url),
-//         ],
-//       ),
-//     );
-//   }
-// }
+List<HeroData> parseHeroData(String responseBody) {
+  final parsedData = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsedData.map<HeroData>((json) => HeroData.fromJson(json)).toList();
+}
+
+class HeroData {
+  final String name;
+  final String url;
+
+  HeroData({this.name, this.url});
+
+  factory HeroData.fromJson(Map<String, dynamic> json) {
+    return HeroData(name: json['name'] as String, url: json['url'] as String);
+  }
+}
