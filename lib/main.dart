1357cd5dd5a03a70d732beacd563bb85dcf43792
SachseInTheHub <nelsonsachse@gmail.com>
2019-02-28 16:08:37 +0000

    Revert "added HeroDetailPage"
    This reverts commit de916f2710ed3568c3808f1339b7412efdfa52ae.

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,80 @@
 import 'package:flutter/material.dart';
-import 'package:pick_a_hero/model/HeroData.dart';
-import 'package:pick_a_hero/page/HeroDetailPage.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter/foundation.dart';
+
+import 'dart:convert';
+import 'dart:async';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Pick A Hero",
-      theme: new ThemeData.dark(),
-      home: new ListPage(title: '🦸‍♀️ Heroes 🦸‍♂️'),
+      title: 'Pick a Hero',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: MyHomePage(
+        header: 'Pick a Hero',
+        url:
+            "https://cdn3.whatculture.com/images/2014/04/SuperiorSpiderMan_02.png",
+      ),
     );
   }
 }
 
-class ListPage extends StatefulWidget {
-  final String title;
-
-  ListPage({Key key, this.title}) : super(key: key);
-
-  @override
-  ListPageState createState() => ListPageState();
-}
-
-class ListPageState extends State<ListPage> {
-  List _heroes;
-
-  @override
-  void initState() {
-    super.initState();
-    _heroes = getHeroes();
-  }
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.header, this.url}) : super(key: key);
+  final String header;
+  final String url;
 
   @override
   Widget build(BuildContext context) {
-    
-    Card createCard(HeroData data) => Card(
-          color: Colors.black87,
-          elevation: 16.0,
-          child: Container(
-              margin:
-                  new EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
-              child: ListTile(
-                title: Text(
-                  data.name,
-                  style: TextStyle(color: Colors.white, fontSize: 30),
-                ),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => DetailPage(data: data)));
-                },
-              )),
-        );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(header),
+      ),
+      body: FutureBuilder<List<HeroData>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) print(snapshot.error);
 
-    final pageBody = Container(
-      child: ListView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: _heroes.length,
-        itemBuilder: (BuildContext context, int index) {
-          return createCard(_heroes[index]);
+          return snapshot.hasData
+              ? ListView(
+                  padding: EdgeInsets.all(8.0),
+                  children: snapshot.data
+                      .map((json) => Image.network(json.url,
+                          height: 200, fit: BoxFit.fitWidth))
+                      .toList())
+              : Center(child: CircularProgressIndicator());
         },
       ),
-    );
-
-    return Scaffold(
-      backgroundColor: Color.fromARGB(200, 100, 100, 130),
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(widget.title),
-      ),
-      body: pageBody,
     );
   }
 }
 
+Future<List<HeroData>> fetchPhotos(http.Client client) async {
+  final response =
+      await client.get('https://api.jsonbin.io/b/5c72ff3f3f18a135eaf15509/4');
+
+  return compute(parseHeroData, response.body);
+}
+
+List<HeroData> parseHeroData(String responseBody) {
+  final parsedData = json.decode(responseBody).cast<Map<String, dynamic>>();
+
+  return parsedData.map<HeroData>((json) => HeroData.fromJson(json)).toList();
+}
+
+class HeroData {
+  final String name;
+  final String url;
+
+  HeroData({this.name, this.url});
+
+  factory HeroData.fromJson(Map<String, dynamic> json) {
+    return HeroData(name: json['name'] as String, url: json['url'] as String);
+  }
+}
